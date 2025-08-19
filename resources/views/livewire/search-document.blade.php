ea8fd18460a255d0b0341156a34a441771bfe7b5
alysson-chrysthian <alyssonchrysthian@gmail.com>
2025-08-19 14:10:58 -0400

    typing error fixed

--- resources/views/livewire/search-document.blade.php
+++ resources/views/livewire/search-document.blade.php
@@ -1,5 +1,5 @@
 <div class="max-w-[500px] m-auto flex flex-col gap-4">
-    <h1 class="text-center">Buscar do documento</h1>
+    <h1 class="text-center">Buscar documento</h1>
 
     <livewire:forms.document-form 
         placeholder="Buscar" 
